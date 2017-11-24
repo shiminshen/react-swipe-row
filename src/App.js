594e90f23373be704338fd1705d0d8aed78aaef5
shiminshen <thanatos1710@gmail.com>
2017-11-24 14:55:42 +0800

    add touch event for swiping

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import SwipeRow from './SwipeRow'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SwipeRow>
+          <span>example 1</span>
+        </SwipeRow>
+        <SwipeRow>
+          <span>example 2</span>
+        </SwipeRow>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/SwipeRow/SwipeRow.js
+++ src/SwipeRow/SwipeRow.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class SwipeRow extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      x: 0,
+      y: 0,
+      move: 0
+    }
+  }
+
+  handleTouchStart = (e) => {
+    this.setState({
+      x: e.targetTouches[0].clientX,
+      y: e.targetTouches[0].clientY
+    })
+  }
+
+  handleTouchEnd = (e) => {
+    this.setState({
+      x: 0,
+      y: 0,
+      move: 0
+    })
+  }
+
+  handleTouchMove = (e) => {
+    this.setState({
+      move: e.targetTouches[0].clientX - this.state.x
+    })
+  }
+
   render () {
+    let {
+      children
+    } = this.props
+
+    console.log(this.state);
     return (
-      <div />
+      <div style={{ width: '100%', overflow: 'hidden' }}>
+        <div
+          className='p-3 bg-light'
+          style={{ position: 'relative', left: this.state.move}}
+          onTouchStart={this.handleTouchStart}
+          onTouchEnd={this.handleTouchEnd}
+          onTouchMove={this.handleTouchMove}
+        >
+          { children }
+        </div>
+      </div>
     )
   }
 }
