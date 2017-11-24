a4671e5f28db5fd659e5d590dcd09c36ce2155cd
shiminshen <thanatos1710@gmail.com>
2017-11-24 16:30:52 +0800

    add callback example to remove item

--- src/SwipeRow/SwipeRow.js
+++ src/SwipeRow/SwipeRow.js
@@ -12,41 +12,44 @@ class SwipeRow extends Component {
     }
   }
 
-  handleTouchStart = (e) => {
+  handleTouchStart = cb => e => {
     this.setState({
       x: e.targetTouches[0].clientX,
       y: e.targetTouches[0].clientY
-    })
+    }, () => cb && cb(this.props.rowId))
   }
 
-  handleTouchEnd = (e) => {
+  handleTouchEnd = cb => e => {
+    console.log(this.props);
     this.setState({
       x: 0,
       y: 0,
       move: 0
-    })
+    }, () => cb && cb(this.props.rowId))
   }
 
-  handleTouchMove = (e) => {
+  handleTouchMove = cb => e => {
     this.setState({
       move: e.targetTouches[0].clientX - this.state.x
-    })
+    }, () => cb && cb(this.props.rowId))
   }
 
   render () {
     let {
+      touchStartCallback,
+      touchEndCallback,
+      touchMoveCallback,
       children
     } = this.props
 
-    console.log(this.state);
     return (
-      <div style={{ width: '100%', overflow: 'hidden' }}>
+      <div style={{ overflow: 'hidden' }}>
         <div
           className='p-3 bg-light'
-          style={{ position: 'relative', left: this.state.move}}
-          onTouchStart={this.handleTouchStart}
-          onTouchEnd={this.handleTouchEnd}
-          onTouchMove={this.handleTouchMove}
+          style={{ position: 'relative', left: this.state.move }}
+          onTouchStart={this.handleTouchStart(touchStartCallback)}
+          onTouchEnd={this.handleTouchEnd(touchEndCallback)}
+          onTouchMove={this.handleTouchMove(touchMoveCallback)}
         >
           { children }
         </div>
