26d92d5822fc3e0708b7e9be454be75a519e0922
Damon Shen <shi-min.shen@oath.com>
2018-01-03 13:52:39 +0800

    add story example

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,8 +5,6 @@ import { action } from '@storybook/addon-actions'
 
 import SwipeRow from '../SwipeRow'
 
-// import '../App.css'
-
 const rightButtons = [
   <div style={{padding: '12px', background: 'blue'}}>Delete</div>,
   <div style={{padding: '12px', background: 'red'}}>Delete</div>,
@@ -14,7 +12,6 @@ const rightButtons = [
 ]
 const leftButtons = [
   <div style={{padding: '12px', background: 'gray'}}>Mute</div>,
-  <div style={{padding: '12px', background: 'orange'}}>Mute</div>,
   <div style={{padding: '12px', background: 'pink'}}>Mute</div>
 ]
 
@@ -29,12 +26,23 @@ storiesOf('SwipeRow', module)
       <div style={{background: '#fff', padding: '12px', textAlign: 'center'}}>example {rowId.toString()}</div>
     </SwipeRow>
   )))
+  .add('Disable parallax', () => [1, 2, 3, 4, 5].map(rowId => (
+    <SwipeRow
+      key={rowId}
+      rowId={rowId}
+      rightButtons={rightButtons}
+      leftButtons={leftButtons}
+      disableParallax
+    >
+      <div style={{background: '#fff', padding: '12px', textAlign: 'center'}}>example {rowId.toString()}</div>
+    </SwipeRow>
+  )))
   .add('Disable swipe right', () => [1, 2, 3, 4, 5].map(rowId => (
     <SwipeRow
       key={rowId}
       rowId={rowId}
-      disableSwipeRight
       rightButtons={rightButtons}
+      disableSwipeRight
     >
       <div style={{background: '#fff', padding: '12px', textAlign: 'center'}}>example {rowId.toString()}</div>
     </SwipeRow>
@@ -43,8 +51,8 @@ storiesOf('SwipeRow', module)
     <SwipeRow
       key={rowId}
       rowId={rowId}
-      disableSwipeLeft
       leftButtons={leftButtons}
+      disableSwipeLeft
     >
       <div style={{background: '#fff', padding: '12px', textAlign: 'center'}}>example {rowId.toString()}</div>
     </SwipeRow>
